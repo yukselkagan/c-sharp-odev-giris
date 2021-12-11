9af98ce799e0f60181d40bfbfa7aa6198c41678c
yukselkagan <yukselkagan2@gmail.com>
2021-12-11 21:58:39 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_sharp_odev_giris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
